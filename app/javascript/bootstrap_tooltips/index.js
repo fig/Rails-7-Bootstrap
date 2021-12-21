ddabf15e79a8df36c822a43e07dd423db10f5532
fig <fig@users.noreply.github.com>
2021-12-21 13:46:42 +0000

    bootstrap

---
+++ app/javascript/bootstrap_tooltips/index.js
@@ -0,0 +1,8 @@
+const bootstrap = require('bootstrap')
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+)
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,58 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  connect () {
+    this.showTime()
+    this.timer = setInterval(() => {
+      this.showTime()
+    }, 1000)
+  }
+
+  showTime () {
+    this.element.textContent = this.currentTimeString()
+  }
+
+  disconnect () {
+    clearInterval(this.timer)
+  }
+
+  //get current time and format as string
+  currentTimeString () {
+    let currentTime = new Date()
+    // day of week in words
+    let dayOfWeek = currentTime.toLocaleString('en-GB', { weekday: 'long' })
+    // day of month
+    let dayOfMonth = currentTime.toLocaleString('en-GB', {
+      day: 'numeric'
+    })
+    // month in words
+    let month = currentTime.toLocaleString('en-GB', { month: 'long' })
+    // year
+    let year = currentTime.getFullYear()
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
+    let seconds = currentTime.getSeconds()
+    let ampm = hours >= 12 ? 'pm' : 'am'
+    hours = hours % 12
+    hours = hours ? hours : 12 // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    return (
+      dayOfWeek +
+      ', ' +
+      dayOfMonth +
+      ' ' +
+      month +
+      ' ' +
+      year +
+      '. ' +
+      hours +
+      ':' +
+      minutes +
+      ':' +
+      seconds +
+      ' ' +
+      ampm
+    )
   }
 }
